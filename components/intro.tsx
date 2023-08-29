07c082890c4a6b219018cc60c334230e59f0fc9d
Andrjuha-joma-jozhik <andrey.zhmudenko1@gmail.com>
2023-08-29 16:19:33 +0300

    #2

--- components/intro.tsx
+++ components/intro.tsx
@@ -56,9 +56,8 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I&apos;m Andriy.</span> I&apos; a{" "}
-        <span className="font-bold">front-end developer</span> with{" "}
-        <span className="font-bold">0 years</span> of experience. I enjoy
+        <span className="font-bold">Hello, I&apos;am Andriy.</span> I&apos;m{" "}
+        <span className="font-bold">front-end developer</span> I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
